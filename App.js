48b73335539e10374ef6d93d8892f91110e0b441
Deep Gabani <16bit052@nirmauni.ac.in>
2023-02-20 00:18:20 +0530

    Added raleway font.

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import MyExpenses from './MyExpenses';
+import AddExpense from './AddExpense';
+import MyProfile from './MyProfile';
+import { useFonts, Raleway_500Medium } from '@expo-google-fonts/raleway';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  let [fontsLoaded] = useFonts({
+    Raleway_500Medium,
+  });
+  
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      initialRouteName="My Expenses"
+      screenOptions={{
+        tabBarActiveTintColor: '#0F172A',
+        tabBarItemStyle: {
+          margin: 6,
+        },
+        tabBarLabelStyle: {
+          fontFamily: 'Raleway_500Medium'
+        },
+        headerShown: false
+      }}
+    >
+      <Tab.Screen
+        name="My Expenses"
+        component={MyExpenses}
+        options={{
+          tabBarLabel: 'My Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bookshelf" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Add Expense"
+        component={AddExpense}
+        options={{
+          tabBarLabel: 'Add Expense',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cash-plus" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="My Profile"
+        component={MyProfile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ MyExpenses.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts, Raleway_300Light, Raleway_500Medium, Raleway_700Bold, Raleway_900Black } from '@expo-google-fonts/raleway';
+
+function MyExpenses() {
+  let [fontsLoaded] = useFonts({
+    Raleway_300Light,
+    Raleway_500Medium,
+    Raleway_700Bold,
+    Raleway_900Black,
+  });
+  
+  if(!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>My Expenses</Text>
+      <Text style={styles.text}>Your expenses will be displayed here!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontFamily: 'Raleway_700Bold'
+  },
+  text: {
+    fontFamily: 'Raleway_300Light'
+  },
+});
+
+export default MyExpenses;
